5651f50287ecec03f51b85ae3b9163e2ca51918e
faulke <faulkev@gmail.com>
2017-09-03 01:06:08 -0600

    Change test reducer to properties

--- src/reducers/properties.js
+++ src/reducers/properties.js
@@ -1,15 +1,19 @@
+import { browserHistory } from 'react-router';
 import * as actions from '../actions';
 
 export const initialState = {
   properties: []
 };
 
-export const testReducer = (state = initialState, { type, payload }) => {
+export const properties = (state = initialState, { type, payload }) => {
   switch (type) {
     case actions.GET_PROPERTIES_REQUEST:
       return { ...state };
     case actions.GET_PROPERTIES_SUCCESS:
       return { ...state, properties: payload };
+    case actions.GET_PROPERTIES_FAILURE:
+      browserHistory.push('account/login');
+      return { ...state };
     default:
       return { ...state };
   }
