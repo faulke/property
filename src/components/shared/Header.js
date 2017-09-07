c446ab9079c8be6a19827108d317e7be935ba32a
faulke <faulkev@gmail.com>
2017-09-07 00:10:37 -0600

    Build header component

---
+++ src/components/shared/Header.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { isLoggedIn, getAuth } from '../../selectors';
+import * as actions from '../../actions';
+
+const Header = ({ ...props }) => (
+  <Navbar inverse>
+    <Navbar.Header>
+      <Navbar.Brand>
+        Logo here
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        {
+          props.isAuthed ? 
+            <NavDropdown id="user-dropdown" title={props.name}>
+              <MenuItem href="/account/login" onClick={props.clearLocalUser}>Log out</MenuItem>
+            </NavDropdown> :
+            <NavItem>Sign In</NavItem>
+        }
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+Header.propTypes = {
+  name: React.PropTypes.string,
+  isAuthed: React.PropTypes.bool.isRequired,
+  clearLocalUser: React.PropTypes.func.isRequired
+};
+
+Header.defaultProps = {
+  name: null
+};
+
+const mapStateToProps = state => ({
+  name: getAuth(state).name,
+  isAuthed: isLoggedIn(state)
+});
+
+export default connect(mapStateToProps, actions)(Header);
