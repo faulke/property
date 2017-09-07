35a7d26500fac82660e0f15ddf25d33bc77488ee
faulke <faulkev@gmail.com>
2017-09-07 00:14:23 -0600

    Add header to index page

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 // This component handles the App template used on every page.
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from './shared/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {this.props.children}
+        <Header />
+        <div>
+          {this.props.children}
+        </div>
       </div>
     );
   }
 }
 
 App.propTypes = {
-  children: PropTypes.object.isRequired,
+  children: PropTypes.object.isRequired
 };
 
 export default App;
