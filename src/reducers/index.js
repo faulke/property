0aac931754aee8e0be648c27caa19b731c660071
faulke <faulkev@gmail.com>
2017-09-07 00:13:18 -0600

    Remove redundant register reducer :smile:

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import { properties } from './properties';
 import { auth } from './auth';
-import { register } from './register';
 import { create } from './createProperty';
 
 const rootReducer = combineReducers({
   properties,
   auth,
-  register,
   create
 });
 
