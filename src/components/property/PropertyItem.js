e2471387c1f9dff6de4596032d2af20cf0d13ee4
faulke <faulkev@gmail.com>
2017-09-12 19:46:19 -0600

    Add placeholder image

--- src/components/property/PropertyItem.js
+++ src/components/property/PropertyItem.js
@@ -4,31 +4,41 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router';
 import styles from './propertyItem.less';
 
-const PropertyItem = ({ ...props }) => (
-  <Col sm={6} className={styles.propertyContainer}>
-    <Grid fluid className={styles.property}>
-      <Row className={styles.imgContainer}>
-        <div className={styles.img}>
-          Image here
-        </div>
-      </Row>
-      <Row className={styles.address}>
-        <Col sm={12}>
-          <h3>{props.address}</h3>
-          <p>{props.city}, {props.state} {props.zipcode}</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={12}>
-          <hr />
-        </Col>
-        <Col sm={12}>
-          Additional info here
-        </Col>
-      </Row>
-    </Grid>
-  </Col>
-);
+const PropertyItem = ({ ...props }) => {
+  const hasHeader = false;
+  const headerImgProp = 'https://d30y9cdsu7xlg0.cloudfront.net/png/7757-200.png';
+  const imgStyle = {
+    backgroundImage: `url(${headerImgProp})`,
+    backgroundSize: hasHeader ? 'cover' : '60px 60px',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center'
+  };
+  return (
+    <Col sm={6} className={styles.propertyContainer}>
+      <Grid fluid className={styles.property}>
+        <Row>
+          <Col sm={12}>
+            <div style={imgStyle} className={styles.img} />
+          </Col>
+        </Row>
+        <Row className={styles.address}>
+          <Col sm={12}>
+            <h3>{props.address}</h3>
+            <p>{props.city}, {props.state} {props.zipcode}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <hr className={styles.hr} />
+          </Col>
+          <Col sm={12}>
+            <p>Additional info here</p>
+          </Col>
+        </Row>
+      </Grid>
+    </Col>
+  );
+};
 
 PropertyItem.propTypes = {
   address: PropTypes.string.isRequired,
