efa7744b4a9553ea21290e825f3d1880e4e8e73d
faulke <faulkev@gmail.com>
2017-09-12 19:46:47 -0600

    Return all properties state

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,4 +1,4 @@
-export const getProperties = state => state.properties.properties;
+export const getProperties = state => state.properties;
 export const getAuth = state => state.auth;
 export const createProperty = state => state.create;
 export const isLoggedIn = state => getAuth(state).name !== null;
