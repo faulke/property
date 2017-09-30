43c58378bc1c7266a21fd6233af15dfb8ec51e00
Evan Faulk <faulkev@gmail.com>
2017-09-30 13:24:14 -0600

    Disable forms during post (#22)
    * Disable forms while posting
    
    * Add isPosting to property form

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = configureStore({
-  auth: { name: localStorage.getItem('name') } 
+  auth: { name: localStorage.getItem('name'), isPosting: false } 
 });
 
 render(
