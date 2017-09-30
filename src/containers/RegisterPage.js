481d7b578db9bba0df68e4bbf926119243206bcd
faulke <faulkev@gmail.com>
2017-09-30 14:29:38 -0600

    Redirect to /properties when registered

--- src/containers/RegisterPage.js
+++ src/containers/RegisterPage.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { browserHistory } from 'react-router';
 import FormTemplate from './shared/FormTemplate';
 import * as actions from '../actions/index';
-import { getAuth } from '../selectors';
+import { getAuth, isLoggedIn } from '../selectors';
 import RegisterForm from '../components/account/RegisterForm';
 
-class RegisterPage extends FormTemplate {
+class RegisterPage extends Component {
   constructor(props) {
     super(props);
 
     this.submit = this.submit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { isAuthed, isPosting } = nextProps;
+    if (isAuthed) {
+      browserHistory.push('/properties');
+    }
   }
 
   submit(values) {
@@ -27,11 +35,13 @@ class RegisterPage extends FormTemplate {
 }
 
 RegisterPage.propTypes = {
-  register: React.PropTypes.func.isRequired
+  register: PropTypes.func.isRequired,
+  isAuthed: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
-  ...getAuth(state)
+  ...getAuth(state),
+  isAuthed: isLoggedIn(state)
 });
 
 export default connect(mapStateToProps, actions)(RegisterPage);
