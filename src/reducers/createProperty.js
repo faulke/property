f84b625ec8d3962e632d358e32f8654066894fec
faulke <faulkev@gmail.com>
2017-09-29 22:06:02 -0600

    Remove browser history action from reducer

--- src/reducers/createProperty.js
+++ src/reducers/createProperty.js
@@ -1,4 +1,3 @@
-import { browserHistory } from 'react-router';
 import * as actions from '../actions';
 
 export const initialState = {
@@ -12,12 +11,9 @@ export const initialState = {
 export const create = (state = initialState, action) => {
   const { type, payload, source, value } = action;
   switch (type) {
-    case actions.UPDATE_INPUT:
-      return { ...state, [source]: value };
     case actions.CREATE_PROPERTY_REQUEST:
       return { ...state };
     case actions.CREATE_PROPERTY_SUCCESS:
-      browserHistory.push('/properties');
       return { ...state };
     default:
       return { ...state };
