8ca6955f9132ce4709fd4276f719240473f4d2f6
faulke <faulkev@gmail.com>
2017-09-29 22:06:10 -0600

    Update form pages

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -10,40 +10,24 @@ class LoginPage extends FormTemplate {
   constructor(props) {
     super(props);
 
-    this.submitForm = this.submitForm.bind(this);
+    this.submit = this.submit.bind(this);
   }
 
-  submitForm(evt) {
-    evt.preventDefault();
-    const { email, password } = this.props;
+  submit(values) {
+    const { email, password } = values;
     this.props.login(email, password);
   }
 
   render() {
     const { email, password } = this.props;
     return (
-      <LoginForm
-        submitForm={this.submitForm}
-        updateForm={this.updateInput}
-        email={email}
-        password={password}
-      />
+      <LoginForm onSubmit={this.submit} />
     );
   }
 }
 
 LoginPage.propTypes = {
-  email: React.PropTypes.string,
-  password: React.PropTypes.string,
-  name: React.PropTypes.string,
-  login: React.PropTypes.func.isRequired,
-  clearLocalUser: React.PropTypes.func.isRequired
-};
-
-LoginPage.defaultProps = {
-  email: '',
-  password: '',
-  name: null
+  login: React.PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
