f83398fb989ba8065a2e7642b77ec03b0b6ad725
Evan Faulk <faulkev@gmail.com>
2017-10-01 18:41:11 -0600

    Add header, save jwt, redirect if api failure (#28)

--- src/middleware/apiAuth.js
+++ src/middleware/apiAuth.js
@@ -1,10 +1,47 @@
-import { ApiError } from 'redux-api-middleware';
+import { isRSAA, CALL_API } from 'redux-api-middleware';
 import { browserHistory } from 'react-router';
 
-// redirect to /login if api returns 401
 export default store => next => action => {
-  if (action.payload && action.payload.status === 401) {
-    browserHistory.replace('/account/login');
+  if (isRSAA(action)) {
+    const actionCopy = { ...action[CALL_API] };
+
+    const headers = actionCopy.headers();
+
+    const token = localStorage.getItem('jwt');
+
+    actionCopy.headers = () => {
+      if (token) {
+        return {
+          ...headers,
+          Authorization: `Bearer ${localStorage.getItem('jwt')}`
+        };
+      }
+      return { ...headers };
+    };
+
+    actionCopy.types = [
+      actionCopy.types[0],
+      {
+        type: actionCopy.types[1],
+        meta: (act, state, res) => {
+          const header = res.headers.get('Authorization');
+          if (header) {
+            const jwt = header.split('Bearer ')[1];
+            localStorage.setItem('jwt', jwt);
+          }
+        }
+      },
+      {
+        type: actionCopy.types[2],
+        meta: () => {
+          // for now, assume only 401 on failure
+          localStorage.removeItem('jwt');
+          browserHistory.push('/account/login');
+        }
+      }
+    ];
+
+    return next({ [CALL_API]: actionCopy });
   }
-  return (next(action));
+  return next(action);
 };
