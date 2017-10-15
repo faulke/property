cd49ee1f03f9023c66e72d5f4564f2c516789948
faulke <faulkev@gmail.com>
2017-10-15 17:38:53 -0600

    Validate form fields

--- src/components/account/RegisterForm.js
+++ src/components/account/RegisterForm.js
@@ -3,31 +3,94 @@ import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
 import { Form, FormControl, FormGroup, ControlLabel, Col, Button } from 'react-bootstrap';
 import FormInput from '../shared/FormInput';
+import styles from './forms.less';
 
-const RegisterForm = ({ handleSubmit, pristine, isPosting }) => (
-  <Form horizontal onSubmit={handleSubmit}>
+const required = value => value ? undefined : 'Required';
+const match = (value, allValues) => value === allValues.password ?
+  undefined :
+  'Passwords do not match.';
+const minLength = value => value.length >= 7 ?
+  undefined :
+  "Password must be at least 7 characters long.";
+
+const uppercase = value => (/(?=.*[A-Z])/).test(value) ?
+  undefined :
+  "Password must contain at least 1 uppercase letter.";
+
+const digit = value => (/(?=.*[0-9])/).test(value) ?
+  undefined :
+  "Password must contain at least 1 number.";
+
+const special = value => (/(?=.*[!@#$%^&*)(])/).test(value) ?
+  undefined :
+  "Password must contain at least 1 special character.";
+
+const passwordHint = `At least 7 characters
+  in length and contains 1 uppercase letter,
+  1 number, and 1 special character.`;
+
+const RegisterForm = ({ handleSubmit, pristine, isPosting, registerError }) => (
+  <Form horizontal onSubmit={handleSubmit} className={styles.form}>
     <Field
       type="text"
+      name="firstName"
+      label="First Name"
+      placeholder="First"
+      component={FormInput}
+      className="form-control"
+      validate={[required]}
+    />
+    <Field
+      type="text"
+      name="lastName"
+      label="Last Name"
+      placeholder="Last"
+      component={FormInput}
+      className="form-control"
+      validate={[required]}
+    />
+    <Field
+      type="email"
       name="email"
       label="Email"
       placeholder="Email"
       component={FormInput}
       className="form-control"
-      required
+      validate={[required]}
     />
     <Field
       type="password"
       name="password"
       label="Password"
+      hint={passwordHint}
       placeholder="Password"
       component={FormInput}
       className="form-control"
-      required
+      validate={[required, minLength, uppercase, digit, special]}
     />
-    <FormGroup>
-      <Col smOffset={4} sm={4}>
-        <Button type="submit" disabled={pristine || isPosting}>
-          Sign in
+    <Field
+      type="password"
+      name="confirmPassword"
+      label="Confirm Password"
+      placeholder="Confirm Password"
+      component={FormInput}
+      className="form-control"
+      validate={[required, match]}
+    />
+    {
+      registerError ?
+        <p className={styles.error}>{registerError}</p> :
+        ''
+    }
+    <FormGroup className={styles.formGroup}>
+      <Col>
+        <Button
+          type="submit"
+          bsStyle={"success"}
+          className={styles.button}
+          disabled={isPosting}
+        >
+          Register
         </Button>
       </Col>
     </FormGroup>
@@ -37,7 +100,12 @@ const RegisterForm = ({ handleSubmit, pristine, isPosting }) => (
 RegisterForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   pristine: PropTypes.bool.isRequired,
-  isPosting: PropTypes.bool.isRequired
+  isPosting: PropTypes.bool.isRequired,
+  registerError: PropTypes.string
+};
+
+RegisterForm.defaultProps = {
+  registerError: null
 };
 
 export default reduxForm({
