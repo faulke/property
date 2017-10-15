aa2f56493ae22a3a09fa662b84af7b3f8b638c7f
faulke <faulkev@gmail.com>
2017-10-15 17:39:13 -0600

    Style register page

--- src/containers/RegisterPage.js
+++ src/containers/RegisterPage.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { browserHistory } from 'react-router';
+import { Grid, Row, Col } from 'react-bootstrap';
 import FormTemplate from './shared/FormTemplate';
 import * as actions from '../actions/index';
 import { getAuth, isLoggedIn } from '../selectors';
 import RegisterForm from '../components/account/RegisterForm';
+import styles from './login.less';
 
 class RegisterPage extends Component {
   constructor(props) {
@@ -22,14 +24,26 @@ class RegisterPage extends Component {
   }
 
   submit(values) {
-    const { email, password } = values;
-    this.props.register(email, password);
+    this.props.register({ ...values });
   }
 
   render() {
-    const { isPosting } = this.props;
+    const { isPosting, registerError } = this.props;
     return (
-      <RegisterForm onSubmit={this.submit} isPosting={isPosting} />
+      <Grid fluid>
+        <Row className={styles.loginHeader}>
+          <h1>Register</h1>
+        </Row>
+        <Row>
+          <Col sm={4} smOffset={4} className={styles.loginContainer}>
+            <RegisterForm 
+              onSubmit={this.submit} 
+              isPosting={isPosting} 
+              registerError={registerError}
+            />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
@@ -37,7 +51,12 @@ class RegisterPage extends Component {
 RegisterPage.propTypes = {
   register: PropTypes.func.isRequired,
   isPosting: PropTypes.bool.isRequired,
+  registerError: PropTypes.string,
   isAuthed: PropTypes.bool.isRequired
+};
+
+RegisterPage.defaultProps = {
+  registerError: null
 };
 
 const mapStateToProps = state => ({
