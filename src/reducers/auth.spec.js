c43b6e836ba2f3d233a41bc7c391e047b10c58be
faulke <faulkev@gmail.com>
2017-10-22 16:42:41 -0600

    Add describe statement

--- src/reducers/auth.spec.js
+++ src/reducers/auth.spec.js
@@ -26,65 +26,68 @@ class LocalStorageMock {
 
 global.localStorage = new LocalStorageMock();
 
-it('should provide the initial state', () => {
-  expect(auth(undefined, {})).toEqual(initialState);
-});
-
-it('should handle GET_AUTH_REQUEST action', () => {
-  const action = {
-    type: actions.GET_AUTH_REQUEST
-  };
-
-  const stateAfter = { ...initialState, isPosting: true };
+describe('auth reducer', () => {
+  it('should provide the initial state', () => {
+    expect(auth(undefined, {})).toEqual(initialState);
+  });
   
-  deepFreeze(initialState);
-  deepFreeze(stateAfter);
-
-  expect(auth(initialState, action)).toEqual(stateAfter);
-});
-
-it('should handle login/register success', () => {
-  const user = 'user name';
-  const action = {
-    type: actions.GET_AUTH_SUCCESS,
-    payload: { user, token: 'token123456' }
-  };
-
-  const stateBefore = { ...initialState, isPosting: true };
-  const stateAfter = { ...stateBefore, isPosting: false, name: user, isLoggedIn: true };
-
-  deepFreeze(stateBefore);
-  deepFreeze(stateAfter);
-
-  expect(auth(stateBefore, action)).toEqual(stateAfter);
-});
-
-it('should handle login auth failure', () => {
-  const action = {
-    type: actions.GET_AUTH_FAILURE
-  };
-
-  const stateBefore = { ...initialState, isPosting: true };
-  const stateAfter = { ...stateBefore, isPosting: false, loginError: 'Email or password is invalid.' };
+  it('should handle GET_AUTH_REQUEST action', () => {
+    const action = {
+      type: actions.GET_AUTH_REQUEST
+    };
   
-  deepFreeze(stateBefore);
-  deepFreeze(stateAfter);
-
-  expect(auth(stateBefore, action)).toEqual(stateAfter);
+    const stateAfter = { ...initialState, isPosting: true };
+    
+    deepFreeze(initialState);
+    deepFreeze(stateAfter);
+  
+    expect(auth(initialState, action)).toEqual(stateAfter);
+  });
+  
+  it('should handle login/register success', () => {
+    const user = 'user name';
+    const action = {
+      type: actions.GET_AUTH_SUCCESS,
+      payload: { user, token: 'token123456' }
+    };
+  
+    const stateBefore = { ...initialState, isPosting: true };
+    const stateAfter = { ...stateBefore, isPosting: false, name: user, isLoggedIn: true };
+  
+    deepFreeze(stateBefore);
+    deepFreeze(stateAfter);
+  
+    expect(auth(stateBefore, action)).toEqual(stateAfter);
+  });
+  
+  it('should handle login auth failure', () => {
+    const action = {
+      type: actions.GET_AUTH_FAILURE
+    };
+  
+    const stateBefore = { ...initialState, isPosting: true };
+    const stateAfter = { ...stateBefore, isPosting: false, loginError: 'Email or password is invalid.' };
+    
+    deepFreeze(stateBefore);
+    deepFreeze(stateAfter);
+  
+    expect(auth(stateBefore, action)).toEqual(stateAfter);
+  });
+  
+  it('should handle register auth failure', () => {
+    const error = 'Duplicate user name.';
+    const action = {
+      type: actions.GET_AUTH_FAILURE,
+      payload: { errors: [{ statusCode: 401, description: error }] }
+    };
+  
+    const stateBefore = { ...initialState, isPosting: true };
+    const stateAfter = { ...stateBefore, isPosting: false, registerError: error };
+  
+    deepFreeze(stateBefore);
+    deepFreeze(stateAfter);
+  
+    expect(auth(stateBefore, action)).toEqual(stateAfter);
+  });
 });
 
-it('should handle register auth failure', () => {
-  const error = 'Duplicate user name.';
-  const action = {
-    type: actions.GET_AUTH_FAILURE,
-    payload: { errors: [{ statusCode: 401, description: error }] }
-  };
-
-  const stateBefore = { ...initialState, isPosting: true };
-  const stateAfter = { ...stateBefore, isPosting: false, registerError: error };
-
-  deepFreeze(stateBefore);
-  deepFreeze(stateAfter);
-
-  expect(auth(stateBefore, action)).toEqual(stateAfter);
-})
