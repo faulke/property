2db582dbe84bdead676233a21fe5560c84a3bf0a
faulke <faulkev@gmail.com>
2017-11-05 13:27:50 -0700

    Update file upload style

--- src/components/shared/FileUpload.js
+++ src/components/shared/FileUpload.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { FormGroup, ControlLabel, Col } from 'react-bootstrap';
 import * as actions from '../../actions';
 import { fileUpload } from '../../selectors';
+import styles from './fileupload.less';
 
 class FileUpload extends Component {
   constructor(props) {
@@ -13,23 +15,31 @@ class FileUpload extends Component {
   }
 
   onDrop(files) {
-    console.log(this.props);
-    this.props.getPresignedUrl(files[0]);
+    this.props.getPresignedUrl(files[0], this.props.uuid);
   }
 
   render() {
+    const { name, label } = this.props;
     return (
-      <div className="dropzone">
-        <Dropzone onDrop={this.onDrop}>
-          <p>Try dropping some files here, or click to select files to upload.</p>
-        </Dropzone>
-      </div>
+      <FormGroup controlId={name} >
+        <Col componentClass={ControlLabel}>
+          {this.props.label}
+        </Col>
+        <Col>
+          <Dropzone onDrop={this.onDrop} className={`${styles.fileupload}`}>
+            <p>Try dropping some files here, or click to select files to upload.</p>
+          </Dropzone>
+        </Col>
+      </FormGroup>
     );
   }
 }
 
 FileUpload.propTypes = {
-  getPresignedUrl: PropTypes.func.isRequired
+  getPresignedUrl: PropTypes.func.isRequired,
+  uuid: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
 };
 
 const mapStateToProps = state => ({
