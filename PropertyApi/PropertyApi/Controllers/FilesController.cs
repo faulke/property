6484af8950e624ab09b971a9136c720f96ee34ea
faulke <faulkev@gmail.com>
2017-11-04 20:10:58 -0600

    Generate pre-signed urls

---
+++ PropertyApi/PropertyApi/Controllers/FilesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.IO;
+using PropertyApi.Models.Property;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PropertyApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class FilesController : Controller
+    {
+        private IAmazonS3 _s3Client { get; set; }
+
+        public FilesController (IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+        // POST api/files
+        // get pre-signed url for each file to upload directly to S3 from client
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Post(FileListModel fileList)
+        {
+            var urls = new List<string>();
+
+            try
+            {
+                foreach (var file in fileList.files)
+                {
+                    var request = new GetPreSignedUrlRequest
+                    {
+                        BucketName = "property-files-dev",
+                        Key = $"{fileList.guid}/{file.FileName}",
+                        Verb = HttpVerb.PUT,
+                        Expires = DateTime.Now.AddMinutes(5)
+                    };
+
+                    var url = _s3Client.GetPreSignedURL(request);
+                    urls.Add(url);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                return BadRequest();
+            }
+
+            return Ok(new { urls });
+        }
+    }
+}
---
+++ PropertyApi/PropertyApi/Models/Property/FileListModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyApi.Models.Property
+{
+    public class FileListModel
+    {
+        public List<IFormFile> files { get; set; }
+        public Guid guid { get; set; }
+    }
+}
