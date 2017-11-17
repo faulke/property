1baa568f43e7cf90b321489fabb78537ce54db6a
faulke <faulkev@gmail.com>
2017-11-17 13:37:01 -0700

    Render file uploads in dropzone component

--- src/components/shared/FileUpload.js
+++ src/components/shared/FileUpload.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { FormGroup, ControlLabel, Col } from 'react-bootstrap';
+import { FormGroup, ControlLabel, Row, Col } from 'react-bootstrap';
 import * as actions from '../../actions';
 import { fileUpload } from '../../selectors';
 import styles from './fileupload.less';
+import FileLoader from '../shared/FileLoader';
 
 class FileUpload extends Component {
   constructor(props) {
@@ -19,15 +20,34 @@ class FileUpload extends Component {
   }
 
   render() {
-    const { name, label } = this.props;
+    const { files, isUploading, input } = this.props;
     return (
-      <FormGroup controlId={name} >
-        <Col componentClass={ControlLabel}>
-          {this.props.label}
-        </Col>
+      <FormGroup controlId={name}>
         <Col>
-          <Dropzone onDrop={this.onDrop} className={`${styles.fileupload}`}>
+          <Dropzone 
+            onDrop={this.onDrop}
+            name={input.name} 
+            className={`${styles.fileupload}`}
+          >
             <p>Try dropping some files here, or click to select files to upload.</p>
+            <Row className={styles.fileRow}>
+              {
+                files.map((file, i) => (
+                  <div 
+                    key={i}
+                    className={styles.filePreview}
+                    name={file.name}
+                    data-index={i}
+                    style={{
+                      backgroundImage: `url(${file.preview})`,
+                      opacity: isUploading ? "0.3" : "1"
+                    }}
+                  >
+                    { isUploading ? <FileLoader /> : '' }
+                  </div>
+                ))
+              }
+            </Row>
           </Dropzone>
         </Col>
       </FormGroup>
@@ -38,8 +58,9 @@ class FileUpload extends Component {
 FileUpload.propTypes = {
   getPresignedUrl: PropTypes.func.isRequired,
   uuid: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired
+  files: PropTypes.array.isRequired,
+  isUploading: PropTypes.bool.isRequired,
+  input: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
