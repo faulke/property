042a4a759a38c9985662f0f7e55c29d8c340632e
faulke <faulkev@gmail.com>
2017-11-19 09:41:37 -0700

    Change isUploading behavior

--- src/reducers/fileUpload.js
+++ src/reducers/fileUpload.js
@@ -11,11 +11,11 @@ const fileUpload = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
     case actions.PRESIGNED_URL_REQUEST:
-      return { ...state, isPosting: true };
+      return { ...state, isUploading: true, isPosting: true };
     case actions.PRESIGNED_URL_SUCCESS:
       return { ...state, isPosting: false, isUploading: true };
     case actions.FILE_UPLOAD_REQUEST:
-      return { ...state, isUploading: true, files: [...state.files, payload.file] };
+      return { ...state, files: [...state.files, payload.file] };
     case actions.FILE_UPLOAD_SUCCESS:
       return { ...state, isUploading: false };
     default:
