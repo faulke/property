b6d5e73e6ba267e28058101885cbcf4e53b98567
faulke <faulkev@gmail.com>
2017-11-22 20:42:10 -0700

    Add property detail header component

---
+++ src/components/property/PropertyDetailHeader.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router';
+import styles from './propertyDetailHeader.less';
+
+const PropertyDetailHeader = ({ address, city, state, zipcode, coverUrl }) => {
+  const hasCoverImage = coverUrl !== null;
+  const coverImage = coverUrl || 'https://d30y9cdsu7xlg0.cloudfront.net/png/7757-200.png';
+  return (
+    <div>
+      <Col sm={1} smHidden />
+      <Col sm={10} className={styles.headerContainer}>
+        <div
+          className={styles.coverImg}
+          style={{ 
+            backgroundImage: `url(${coverUrl})`,
+            backgroundSize: hasCoverImage ? 'cover' : '60px 60px',
+          }}
+        />
+        <div className={styles.address}>
+          <h2>{address}</h2>
+          <div>{city}, {state} {zipcode}</div>
+        </div>
+        <div className={styles.backButton}>
+          <Link to={"/properties"} className={"btn btn-default"}>
+            <div><i className={styles.arrowLeft} /> All properties</div>
+          </Link>
+        </div>
+      </Col>
+    </div>
+  );
+};
+
+PropertyDetailHeader.propTypes = {
+  address: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+  zipcode: PropTypes.number.isRequired,
+  coverUrl: PropTypes.string
+};
+
+PropertyDetailHeader.defaultProps = {
+  coverUrl: null
+};
+
+export default PropertyDetailHeader;
