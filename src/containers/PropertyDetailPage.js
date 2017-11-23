d208cfe5643b647368d1f97d36755f495f692314
faulke <faulkev@gmail.com>
2017-11-22 20:43:18 -0700

    Add tabs navigation to property detail

--- src/containers/PropertyDetailPage.js
+++ src/containers/PropertyDetailPage.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Grid, Row, Col, Tabs, Tab } from 'react-bootstrap';
 import * as actions from '../actions';
 import { propertyDetail } from '../selectors';
 import s3Url from '../utils/s3Url';
 import Loader from '../components/shared/Loader';
+import PropertyDetailHeader from '../components/property/PropertyDetailHeader';
+import styles from './propertyDetail.less';
 
 class PropertyDetailPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleTabSelect = this.handleTabSelect.bind(this);
+
+    this.state = {
+      key: 1
+    };
+  }
+
   componentWillMount() {
     const { id } = this.props.params;
     this.props.fetchPropertyDetail(id);
+  }
+
+  handleTabSelect(key) {
+    this.setState({ key });
   }
 
   render() {
@@ -21,21 +38,29 @@ class PropertyDetailPage extends Component {
     return (
       isFetching ?
         <Loader /> :
-        <div>
-          <div>{details.address}</div>
-          <div>{details.city}</div>
-          <div>{details.state}</div>
-          <div>{details.zipcode}</div>
-          <div>{details.rent}</div>
-          {
-            details.files.map((x, i) => {
-              const url = s3Url(x);
-              return (
-                <img alt="property" key={i} src={url} style={{ height: '100px', width: '100px' }} />
-              );
-            })
-          }
-        </div>
+        <Grid fluid style={{ flex: "auto" }}>
+          <Row style={{ margin: "10px 0px 10px 0px" }}>
+            <PropertyDetailHeader
+              address={details.address}
+              city={details.city}
+              state={details.state}
+              zipcode={details.zipcode}
+              coverUrl={s3Url(details.files[0]) || null}
+            />
+          </Row>
+          <Row>
+            <Tabs
+              activeKey={this.state.key} 
+              onSelect={this.handleTabSelect}
+              animation={false}
+              id="property-detail-tabs"
+            >
+              <Tab eventKey={1} title="Payments">Payments</Tab>
+              <Tab eventKey={2} title="Listing">Listing</Tab>
+              <Tab eventKey={3} title="Applications">Applications</Tab>
+            </Tabs>
+          </Row>
+        </Grid>
     );
   }
 
