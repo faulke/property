8063ab911305a9127e2d8101087f71956dfa19f7
Evan Faulk <faulkev@gmail.com>
2019-08-29 21:32:57 -0600

    Notifications button

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Navbar, Nav, Dropdown } from 'rsuite';
+import { Navbar, Nav, Dropdown, Icon, Popover, Whisper, Button } from 'rsuite';
 import { browserHistory, Link } from 'react-router';
 import { isLoggedIn, getAuth } from '../../selectors';
 import * as actions from '../../actions';
 import styles from './header.less';
+
+const Speaker = ({ ...props }) => (
+  <Popover title="Notifications" {...props}>
+    <ul>
+      <li>Notification 1</li>
+      <li>Notification 2</li>
+    </ul>
+  </Popover>
+);
+
+const Notifications = () => (
+  <Whisper
+    trigger="click"
+    placement="bottomRight"
+    speaker={<Speaker />}
+  >
+    <Button appearance="primary" className={styles.notifications}>
+      <Icon icon="bell-o" />  
+    </Button>
+  </Whisper>
+);
 
 const Header = ({ name, logout, isAuthed }) => (
   <Navbar appearance="inverse"> 
@@ -18,11 +39,17 @@ const Header = ({ name, logout, isAuthed }) => (
     <Navbar.Body>
       <Nav pullRight>
         {
-          isAuthed ? 
-            <Dropdown id="user-dropdown" title={name}>
-              <Dropdown.Item>Profile</Dropdown.Item>
-              <Dropdown.Item onClick={logout}>Log out</Dropdown.Item>
-            </Dropdown> :
+          isAuthed ? (
+            <div>
+              <Nav.Item
+                componentClass={Notifications}
+              />
+              <Dropdown id="user-dropdown" title={name}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+                <Dropdown.Item onClick={logout}>Log out</Dropdown.Item>
+              </Dropdown>
+            </div>
+          ) :
             <Nav.Item 
               onClick={() => browserHistory.push('/account/login')}
             >Sign In</Nav.Item>
